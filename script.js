6ce4238bafcf07f984da336de96dedc0280e3c92
codingtest11 <115594653+codingtest11@users.noreply.github.com>
2022-10-14 10:08:50 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const dropDownMenu = document.getElementsByClassName('nav_box')[0];
-const burgerMenu = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', () => {
-    dropDownMenu.classList.toggle('active')
-})
-
-// playbar
