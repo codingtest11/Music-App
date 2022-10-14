a4bd748e89b5678b3b118b85252d194fff50c38e
codingtest11 <115594653+codingtest11@users.noreply.github.com>
2022-10-14 15:17:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const dropDownMenu = document.getElementsByClassName('nav_box')[0];
+const burgerMenu = document.getElementById('menu');
+const close = document.getElementsByClassName('close')[0];
+const slider = document.querySelector('.slider');
+
+burgerMenu.addEventListener('click', () => {
+    dropDownMenu.classList.toggle('active')
+})
+
+close.addEventListener('click', () => {
+    slider.classList.toggle('active')
+})
+
+window.onscroll = function() {myFunction()};
+const navbar = document.querySelector('.topBar-bg');
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    if(window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky')
+    }else{
+        navbar.classList.remove('sticky');
+    }
+}
